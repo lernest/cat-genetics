0672d8f7bb1c3fb4429668432bf43ff414cd1c9b
lernest <19194046+lernest@users.noreply.github.com>
2022-12-27 21:13:24 -0800

    pick samples

--- index.js
+++ index.js
@@ -6,16 +6,23 @@
     Punnet Square
 
          X      x
-    X |--------------
-      |  XX  |   Xx
+      |--------------
+    X |  XX  |   Xx
       |--------------
     x |  Xx  |   xx
       |--------------
 
 */
 
-// params: alleleOne [X,x], alleleTwo [X,x]
-// returns: punnetSquare [[X,X],[X,x],[X,x],[x,x]]
+
+/*  
+    params: alleleOne [X,x], alleleTwo [X,x]
+    returns: punnetSquare {
+                left: alleleOne,
+                right: alleleTwo,
+                squares: [[X,X],[X,x],[X,x],[x,x]]
+            }
+*/
 function generatePunnet(alleleOne, alleleTwo){
     let punnetSquare = {}
 
@@ -23,18 +30,31 @@ function generatePunnet(alleleOne, alleleTwo){
     punnetSquare.left = alleleOne
     punnetSquare.right = alleleTwo
 
+    // Generate square
     punnetSquare.squares = []
     alleleOne.forEach(x => {
-        alleleTwo.forEach(y =>punnetSquare.squares.push([x,y]))
+        // Pair each element with every other element. Sort to keep uppercase first
+        alleleTwo.forEach(y =>punnetSquare.squares.push([x,y].sort()))
     })
 
     return punnetSquare
 }
 
-function printPunnet({left, right, squares}){
-    // Always print uppercase first
-    squares = squares.map(x=>x.sort())
-    
+/*
+    params: a punnet square, but we only need squares[]
+    returns: an allele [X,x]
+*/
+function pickSample({squares}){
+    // Generate random number 0-3
+    let randomNum = Math.floor(Math.random()*4)
+
+    return squares[randomNum]
+}
+
+/*
+    params: a punnet square
+*/
+function printPunnet({left, right, squares}){    
     let strBuffer = '************************************\n'
     strBuffer += `          ${right[0]}          ${right[1]}\n`
     strBuffer += `    -------------------------\n`
@@ -48,4 +68,33 @@ function printPunnet({left, right, squares}){
     console.log(strBuffer)
 }
 
-printPunnet(generatePunnet(['X','x'],['X','x']))
+// function printUnlimited({left, right, squares}){
+//     // print top row
+//     let strBuffer = '    '
+//     right.forEach(x => {
+//         strBuffer += `      ${x}      `
+//     })
+//     strBuffer += '\n    '
+
+//     // print line
+//     right.forEach(x => strBuffer += '-------------')
+//     strBuffer += '\n    '
+
+//     // left.forEach(x => {
+//     //     strBuffer += `${x} |`
+//     //     right.forEach
+//     // })
+//     console.log(strBuffer)
+    
+// }
+
+
+
+console.log('Generating punnet square...')
+let punnet = generatePunnet(['A','a'],['A','a'])
+printPunnet(punnet)
+
+console.log('Picking samples...')
+for(let i=0; i<15; i++){
+    console.log(pickSample(punnet))
+}
